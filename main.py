894ac9ddab2903d88b385643516837ff6d88fc68
sakakendo <sakakendo0321@gmail.com>
2018-03-13 02:32:14 +0900

    fix some

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ sudo=""
 "\n"
 ""
 
+#def commit2filename(
+
 def id2name(d):
-#    print('id2name',d)#,split(uid)[0])
     if d.get('user'):
         ret=sc.api_call('users.info',user=d.get('user'))
         logging.info('this is user'+ret.get('user').get('name'))
@@ -34,36 +35,49 @@ def id2name(d):
 def extract(d):
     attach=d.get('attachments')
     if attach is not None and len(attach) > 0 : text=attach[0].get('text')
-    print(text)
     return text
 
 def reply(d):
-#    print('reply',d)
+    last=[None for i in range(3)]
     print('name : ',id2name(d))
     if id2name(d) == 'github':
-        if extract(d) is not None and re.search('Add files via upload',extract(d)):
-            sc.api_call('chat.postMessage',channel=d.get('channel'),text='you should change commit message')
+        text=extract(d)
+        if text is not None and re.search('Add files via upload',text):
+            sc.api_call('chat.postMessage',channel=d.get('channel'),text='you should specific commit message',thread_ts=d.get('ts'))
+            for txt in re.split('[/\|:]',text) :
+                if last[:0] == 'commit' : print('commit',last[-3:-2],last[:0])
+                elif last[:0]=='compare': print('comp',last)#comp=txt
+                last.append(txt)
+                '''
+                last[2]=last[1]
+                last[1]=last[0]
+                last[0]=txt
+                '''
+#print('comp,comm:',comp,comm)
 
-            
 
-if sc.rtm_connect(with_team_state=True):
-    while True:
-        data=sc.rtm_read()
-        if data == []:
-            pass
-        else:
-            for d in data:
-                print(d)
-                if d['type'] == 'message' and d.get('subtype') is None:
-                    if d.get('username') is 'sakakendobot' :
-                        print('this is bot message')
-                    else:
-                        reply(d)
-                elif d['type'] == 'hello':
-                    sc.api_call(
-                            'chat.postMessage',
-                            channel='dev',
-                            text='bot added')
-        time.sleep(1)
-else:
-    print('Connection Failed')
+def main():
+    if sc.rtm_connect(with_team_state=True):
+        while True:
+            data=sc.rtm_read()
+            if data == []:
+                pass
+            else:
+                for d in data:
+                    print(d)
+                    if d['type'] == 'message' and d.get('subtype') is None:
+                        if d.get('username') is 'sakakendobot' :
+                            print('this is bot message')
+                        else:
+                            reply(d)
+                    elif d['type'] == 'hello':
+                        sc.api_call(
+                                'chat.postMessage',
+                                channel='dev',
+                                text='bot added')
+            time.sleep(1)
+    else:
+        print('Connection Failed')
+
+if __name__ == '__main__':
+    main()
